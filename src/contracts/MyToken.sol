f2b1881effded9f77ae3761b468512c113eb0845
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-09-25 10:04:48 -0400

    added an _mint function

--- src/contracts/MyToken.sol
+++ src/contracts/MyToken.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MyToken is ERC20 {
 
     constructor() ERC20("Romulon","ROM") {
-        
+        _mint(msg.sender, 1000000);
     }
 }
