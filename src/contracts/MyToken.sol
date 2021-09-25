61af5f5ab6a1844a9f72a381e63b664a13aa3346
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-09-25 09:49:54 -0400

    added openzeppelin import

--- src/contracts/MyToken.sol
+++ src/contracts/MyToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract MyToken {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-constructor(uint256 initialSupply) public ERC20("#NAME","#SYMBOL") {
-    
-}
+contract MyToken is ERC20 {
 
-
+    constructor() ERC20("","") {
+        
+    }
 }
