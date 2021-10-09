790a06b03a815ad55822269f49429b72463d887b
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-10-09 09:48:39 -0400

    updatd mytoken and ran comiplers

--- src/contracts/MyToken.sol
+++ src/contracts/MyToken.sol
@@ -5,7 +5,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
 
+    address public admin;
+
     constructor() ERC20("Romulon","ROM") {
         _mint(msg.sender, 1000000);
+        admin = msg.sender;
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == admin, 'only admin');
+        _mint(to, amount);
     }
 }
